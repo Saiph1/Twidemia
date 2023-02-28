4d179f9878055f228b001bc4ab2edfad2d73a90b
Jennifer Wang <1155136569@link.cuhk.edu.hk>
2023-02-28 21:34:18 +0800

    feed section, home bar

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Sidebar from '@/components/Sidebar'
+import Feed from '@/components/Feed'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,7 +25,8 @@ export default function Home() {
 
 
         {/* Feed */}
-
+        <Feed/>
+        
         {/* Widgets */}
 
 
