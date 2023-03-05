e98548859a741bde934bf9694947e9f437529236
Jennifer Wang <1155136569@link.cuhk.edu.hk>
2023-03-05 18:30:39 +0800

    js to jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+
 import Sidebar from '@/components/Sidebar'
 import Feed from '@/components/Feed'
 import Widgets from '@/components/Widgets'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -23,7 +22,7 @@ export default function Home() {
 
         {/* Sidebar */}
         <Sidebar />
-
+        
 
         {/* Feed */}
         <Feed/>
