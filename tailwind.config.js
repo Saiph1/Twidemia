d96ef9841393a8bbae124b5a9f0f50221fe8c647
sky <mng0470@gmail.com>
2023-03-16 23:58:26 +0800

    reinstall autoformat package

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  important: '#__next',
+  important: "#__next",
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
