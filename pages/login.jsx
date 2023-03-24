4696db5c01a8e24cc3e7d3b1d2675b37f4ca8cc5
LC_Hsuan <jackie6388@hotmail.com.tw>
2023-03-24 18:20:45 +0800

    Minor changes to the UI

--- pages/login.jsx
+++ pages/login.jsx
@@ -60,10 +60,10 @@ export default function Login({ csrfToken, error, providers }) {
         <main class="" id="container">
           <section class="bg-gray-50 dark:bg-gray-900">
             <button onClick={()=>{setDark(!Dark); handledark()}}>
-              {(Dark) && <svg display="inline-block" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6" style={{margin: '20px', float:'right'}}>
+              {(Dark) && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6" style={{margin: '20px'}}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                 </svg>}
-              {(!Dark) && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6" style={{margin: '20px', float:'right'}}>
+              {(!Dark) && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6" style={{margin: '20px'}}>
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
                 </svg>}
             </button>
@@ -75,7 +75,7 @@ export default function Login({ csrfToken, error, providers }) {
               
               <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                  <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                  <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white" style={{display: 'flex', alignItems:'center', justifyContent:'center'}}>
                     Login in to your account
                   </h1>
                   <form
@@ -163,8 +163,8 @@ export default function Login({ csrfToken, error, providers }) {
                       Login
                     </button>
 
-                    <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet?{" "}
+                    <p class="text-sm font-light text-gray-500 dark:text-gray-400" style={{display: 'flex', alignItems:'center', justifyContent:'center'}}>
+                      Don’t have an account yet? &nbsp; {" "}
                       <Link
                         href="/signup"
                         class="font-medium text-primary-600 hover:underline dark:text-white"
