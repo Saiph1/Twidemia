3e4e4d1055670ee4d7381a64de6e7ac180f3b48e
LC_Hsuan <75319087+Saiph1@users.noreply.github.com>
2023-03-25 06:10:03 +0800

    Update edit button

--- components/ProfileContainer.jsx
+++ components/ProfileContainer.jsx
@@ -76,7 +76,11 @@ export default function ProfileContainer() {
             alignItems: "flex-start",
           }}
           >
-          <Button size="small" onClick={handleClickOpen}>
+          <Button 
+          class="bg-white hover:bg-gray-100 text-blue-500 py-2 px-4 border border-gray-300 rounded shadow" 
+          size="small" 
+          onClick={handleClickOpen}
+          >
             Edit profile
           </Button>
         </CardActions>
