cdf8590e17535d3e8769880be2bd240053c286c6
LC_Hsuan <75319087+Saiph1@users.noreply.github.com>
2023-03-25 08:46:51 +0800

    Changes to wording and UI

--- pages/signup.jsx
+++ pages/signup.jsx
@@ -31,6 +31,7 @@ export default function Signup() {
 
   const [message, setMessage] = useState("");
   const [error, setError] = useState(false);
+  const [Dark, setDark] = useState(true);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -66,6 +67,10 @@ export default function Signup() {
     }
   }
 
+  function handledark(){
+    document.getElementById("container").className = Dark? "dark": "";
+  }
+
   if (status === "loading") {
     return <></>;
   } else if (status === "authenticated") {
@@ -78,8 +83,16 @@ export default function Signup() {
           <link rel="icon" href="/Twidemia-logo.png" />
         </Head>
 
-        <main>
+        <main class="" id="container">
           <section class="bg-gray-50 dark:bg-gray-900">
+            <button onClick={()=>{setDark(!Dark); handledark()}}>
+              {(Dark) && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="black" className="w-6 h-6" style={{margin: '20px'}}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+                </svg>}
+              {(!Dark) && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="w-6 h-6" style={{margin: '20px'}}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
+                </svg>}
+            </button>
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
               <a
                 href="#"
@@ -106,7 +119,7 @@ export default function Signup() {
                         name="email"
                         id="email"
                         class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="name@company.com"
+                        placeholder="StudentID@link.cuhk.edu.hk"
                         required
                       />
                     </div>
@@ -152,7 +165,7 @@ export default function Signup() {
                     )}
                     <button
                       type="submit"
-                      class="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center border dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                      class=" w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center border dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 dark:text-white"
                     >
                       Sign up
                     </button>
