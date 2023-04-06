9a25ffccebee253901d58103e426cd74c01920ae
Jennifer Wang <1155136569@link.cuhk.edu.hk>
2023-04-06 18:26:03 +0800

    Update [userid].js

--- pages/api/user/[userid].js
+++ pages/api/user/[userid].js
@@ -21,5 +21,42 @@ export default async function handler(req, res) {
     default:
       res.status(400).json({ success: false });
       break;
+
+    
+    case "PUT":
+    let updateUser = req.body;  
+    console.log(updateUser);
+    try {
+        const user = await User.findOne({ userId: req.query.userid });
+        // When user exist, update user info
+        if (user) {
+          // console.log(user);
+
+          // // Find the user in the user database first.
+          // let user = await User.findOne({_id: req.query.userid});
+          // if (!user) {
+          //   res.status(404).json({error: "User not found"});
+          // }
+
+          // Update user
+          // Update the event information.
+          if (updateUser.username) user.username = updateUser.username;
+          if (updateUser.description) user.Description = updateUser.description;
+          if (updateUser.facultyValue) user.faculty = updateUser.facultyValue;
+          
+          // Save and return.
+          await user.save();
+          res.status(200).json(user);
+
+        } else res.status(404).json({error: "User not found."});
+      
+      } catch (error) {
+        //Return 500 if unsuccessful.
+        console.log(error);
+        res.status(500).json({error: error.toString()});
+      }
+      break;
+
+
   }
 }
